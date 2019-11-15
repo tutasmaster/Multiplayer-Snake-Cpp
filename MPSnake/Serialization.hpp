27a64dd13c94629c52d99d7f768c4a494f8a2a1f
Rui Simões <ruisims@gmail.com>
2019-11-15 22:33:44 +0000

    Updated Serialization class

--- MPSnake/Serialization.hpp
+++ MPSnake/Serialization.hpp
@@ -28,10 +28,13 @@ namespace Serial {
 			return _packet;
 		}
 
+		Packet& operator<<(float d) { data.push_back(((long)f & 0xff)); data.push_back(((long)f & 0xff00) >> 8); data.push_back(((long)f & 0xff0000) >> 16); data.push_back(((long)f & 0xff000000) >> 24); return *this; }
 		Packet& operator<<(unsigned char d) { data.push_back(d); return *this; }
 		Packet& operator<<(unsigned short d)	{ data.push_back(d & 0xFF); data.push_back((d & 0xFF00) >> 8); return *this; }
+		Packet& operator<<(unsigned int d) { data.push_back(d & 0xFF); data.push_back((d & 0xFF00) >> 8); data.push_back((d & 0xFF0000) >> 16); data.push_back((d & 0xFF000000) >> 24); return *this; }
 		Packet& operator<<(char d)				{ data.push_back(d); return *this; }
 		Packet& operator<<(short d)			{ data.push_back(d & 0xFF); data.push_back((d & 0xFF00) >> 8); return *this; }
+		Packet& operator<<(int d) { data.push_back(d & 0xFF); data.push_back((d & 0xFF00) >> 8); data.push_back((d & 0xFF0000) >> 16); data.push_back((d & 0xFF000000) >> 24); return *this; }
 		Packet& operator<<(std::string d) { 
 			for (auto i = 0; i < d.size(); i++) { 
 				data.push_back(d.at(i)); 
@@ -41,10 +44,13 @@ namespace Serial {
 			return *this;
 		}
 
+		Packet& operator>>(float& d) { long temp = data[cur] + (data[cur + 1] << 8) + (data[cur + 2] << 16) + (data[cur + 3] << 24); d = (float)temp; cur += 4; return *this; }
 		Packet& operator>>(unsigned char& d)	{ d = data[cur]; cur++; return *this; }
 		Packet& operator>>(unsigned short& d)	{ d = data[cur] + (data[cur + 1] << 8); cur += 2; return *this; }
+		Packet& operator>>(unsigned int& d) { d = data[cur] + (data[cur + 1] << 8) + (data[cur + 2] << 16) + (data[cur + 3] << 24); cur += 4; return *this; }
 		Packet& operator>>(char& d)			{ d = data[cur]; cur++; return *this; }
 		Packet& operator>>(short& d)			{ d = data[cur] + (data[cur + 1] << 8); cur += 2; return *this; }
+		Packet& operator>>(int& d) { d = data[cur] + (data[cur + 1] << 8) + (data[cur + 2] << 16) + (data[cur + 3] << 24); cur += 4; return *this; }
 		Packet& operator>>(std::string& d) {
 			while (data[cur] != '\0') {
 				d += data[cur];
